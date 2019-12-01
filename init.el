56bbd68d734df393563ee604ff34e906a392d8fe
Boris Shminke <boris.shminke.ml>
2019-12-01 11:40:17 +0300

    enable column number mode by default

--- init.el
+++ init.el
@@ -5,6 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(before-save-hook (quote (py-isort-before-save)))
+ '(column-number-mode t)
  '(company-auto-complete t)
  '(company-backends
    (quote
