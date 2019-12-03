e8acbb84459e259156e4ab9709d218edfe5b0963
Boris Shminke <boris.shminke.ml>
2019-12-03 09:09:01 +0300

    use XeTeX by default

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-engine (quote xetex))
  '(before-save-hook (quote (py-isort-before-save)))
  '(column-number-mode t)
  '(company-auto-complete t)
