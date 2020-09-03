b99ee0eb7b95641ca42367263b86d9b3abb96229
Boris Shminke <boris.shminke.ml>
2020-09-03 21:16:04 +0300

    add telega

--- init.el
+++ init.el
@@ -4,82 +4,74 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-engine (quote xetex))
- '(before-save-hook (quote (py-isort-before-save elpy-black-fix-code)))
+ '(TeX-engine 'xetex)
+ '(before-save-hook '(py-isort-before-save elpy-black-fix-code))
  '(calendar-latitude 55.75222)
  '(calendar-longitude 37.61556)
  '(calendar-week-start-day 1)
  '(column-number-mode t)
+ '(company-auto-commit t)
  '(company-auto-complete t)
  '(company-backends
-   (quote
-    (company-bbdb company-eclim company-semantic company-clang company-xcode company-cmake company-capf company-files
+   '(company-bbdb company-eclim company-semantic company-clang company-xcode company-cmake company-capf company-files
 		  (company-dabbrev-code company-gtags company-etags company-keywords)
-		  company-oddmuse company-dabbrev company-jedi)))
+		  company-oddmuse company-dabbrev company-jedi))
  '(counsel-mode t)
- '(custom-enabled-themes (quote (manoj-dark)))
- '(default-frame-alist (quote ((fullscreen . maximized))))
+ '(custom-enabled-themes '(manoj-dark))
+ '(default-frame-alist '((fullscreen . maximized)))
  '(disable-mouse-global-mode t nil (disable-mouse))
  '(ediff-highlight-all-diffs nil)
- '(ediff-split-window-function (quote split-window-vertically))
- '(ediff-window-setup-function (quote ediff-setup-windows-plain))
- '(ein:completion-backend (quote ein:use-company-backend))
- '(ein:notebook-modes (quote (ein:notebook-python-mode)))
+ '(ediff-split-window-function 'split-window-vertically)
+ '(ediff-window-setup-function 'ediff-setup-windows-plain)
+ '(ein:completion-backend 'ein:use-company-backend)
+ '(ein:notebook-modes '(ein:notebook-python-mode))
  '(electric-operator-enable-in-docs t)
- '(elpy-mode-hook (quote (flycheck-mode diff-hl-margin-mode)))
+ '(elpy-mode-hook '(flycheck-mode diff-hl-margin-mode))
  '(elpy-modules
-   (quote
-    (elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-sane-defaults)))
- '(elpy-rpc-virtualenv-path (quote current))
- '(elpy-test-pytest-runner-command (quote ("py.test")))
- '(elpy-test-runner (quote elpy-test-pytest-runner))
+   '(elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-sane-defaults))
+ '(elpy-rpc-virtualenv-path 'current)
+ '(elpy-test-pytest-runner-command '("py.test"))
+ '(elpy-test-runner 'elpy-test-pytest-runner)
  '(enable-recursive-minibuffers t)
  '(eshell-rc-script "~/.zshenv")
- '(exec-path-from-shell-variables (quote ("PATH" "MANPATH" "LC_ALL" "LANG")))
+ '(exec-path-from-shell-variables '("PATH" "MANPATH" "LC_ALL" "LANG"))
  '(explicit-shell-file-name "/bin/zsh")
  '(fill-column 69)
  '(global-diff-hl-mode t)
  '(global-hungry-delete-mode t)
  '(global-subword-mode t)
- '(gnus-select-method (quote (nnimap "imap.yandex.ru")))
+ '(gnus-select-method '(nnimap "imap.yandex.ru"))
  '(grep-find-ignored-directories
-   (quote
-    ("SCCS" "RCS" "CVS" "MCVS" ".src" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" ".mypy_cache" "venv" "build" "_build" ".coverage" ".pytest_cache" "test-results" "dist")))
+   '("SCCS" "RCS" "CVS" "MCVS" ".src" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" ".mypy_cache" "venv" "build" "_build" ".coverage" ".pytest_cache" "test-results" "dist"))
  '(ivy-mode t)
- '(keyboard-coding-system (quote utf-8-emacs))
- '(magit-log-mode-hook (quote (hl-line-mode)))
+ '(keyboard-coding-system 'utf-8-emacs)
+ '(magit-log-mode-hook '(hl-line-mode))
  '(magit-status-sections-hook
-   (quote
-    (magit-insert-status-headers magit-insert-merge-log magit-insert-rebase-sequence magit-insert-am-sequence magit-insert-sequencer-sequence magit-insert-bisect-output magit-insert-bisect-rest magit-insert-bisect-log magit-insert-untracked-files magit-insert-unstaged-changes magit-insert-staged-changes magit-insert-stashes magit-insert-unpushed-to-pushremote magit-insert-unpushed-to-upstream-or-recent magit-insert-unpulled-from-pushremote magit-insert-unpulled-from-upstream hl-line-mode)))
+   '(magit-insert-status-headers magit-insert-merge-log magit-insert-rebase-sequence magit-insert-am-sequence magit-insert-sequencer-sequence magit-insert-bisect-output magit-insert-bisect-rest magit-insert-bisect-log magit-insert-untracked-files magit-insert-unstaged-changes magit-insert-staged-changes magit-insert-stashes magit-insert-unpushed-to-pushremote magit-insert-unpushed-to-upstream-or-recent magit-insert-unpulled-from-pushremote magit-insert-unpulled-from-upstream hl-line-mode))
  '(menu-bar-mode nil)
  '(minibuffer-depth-indicate-mode t)
- '(ns-function-modifier (quote hyper))
+ '(ns-function-modifier 'hyper)
  '(nyan-mode t)
  '(org-agenda-files (list org-default-notes-file))
  '(org-capture-templates
-   (quote
-    (("t" "Task" entry
+   '(("t" "Task" entry
       (file+headline org-default-notes-file "Inbox")
       "** TODO %? [/]
    :CAPTURED: %U
-"))))
+")))
  '(org-format-latex-options
-   (quote
-    (:foreground default :background default :scale 3.0 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
-		 ("begin" "$1" "$" "$$" "\\(" "\\["))))
+   '(:foreground default :background default :scale 3.0 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
+		 ("begin" "$1" "$" "$$" "\\(" "\\[")))
  '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello))
- '(org-trello-files (quote ("~/.trello.org")) nil (org-trello))
+ '(org-trello-files '("~/.trello.org") nil (org-trello))
  '(package-archives
-   (quote
-    (("gnu" . "https://elpa.gnu.org/packages/")
+   '(("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/")
-     ("org" . "https://orgmode.org/elpa/"))))
+     ("org" . "https://orgmode.org/elpa/")))
  '(package-selected-packages
-   (quote
-    (org-trello lsp-java sbt-mode scala-mode lsp-ui lsp-ivy lsp-metals avy which-key electric-operator fireplace diff-hl hungry-delete dictionary f cdlatex move-text elpy zone-nyan nyan-mode xclip restart-emacs exec-path-from-shell disable-mouse ein ws-butler smartparens flycheck company-jedi docker py-isort counsel-projectile counsel dockerfile-mode realgud yaml-mode magit projectile markdown-mode)))
+   '(telega org-trello lsp-java sbt-mode scala-mode lsp-ui lsp-ivy lsp-metals avy which-key electric-operator fireplace diff-hl hungry-delete dictionary f cdlatex move-text elpy zone-nyan nyan-mode xclip restart-emacs exec-path-from-shell disable-mouse ein ws-butler smartparens flycheck company-jedi docker py-isort counsel-projectile counsel dockerfile-mode realgud yaml-mode magit projectile markdown-mode))
  '(projectile-after-switch-project-hook
-   (quote
-    ((lambda nil
+   '((lambda nil
        (progn
 	 (setq venv-folder
 	       (f-join
@@ -89,23 +81,22 @@
 	     (file-directory-p venv-folder)
 	     (pyvenv-activate venv-folder)
 	   (setenv "PYTHONPATH"
-		   (projectile-project-root))))))))
+		   (projectile-project-root)))))))
  '(projectile-before-switch-project-hook
-   (quote
-    ((lambda nil
+   '((lambda nil
        (progn
 	 (pyvenv-deactivate)
-	 (setenv "PYTHONPATH" nil))))))
- '(projectile-completion-system (quote ivy))
+	 (setenv "PYTHONPATH" nil)))))
+ '(projectile-completion-system 'ivy)
  '(projectile-mode t nil (projectile))
- '(projectile-project-search-path (quote ("~/projects")))
+ '(projectile-project-search-path '("~/projects"))
  '(python-check-command "pylint")
  '(python-shell-completion-native-enable nil)
  '(pyvenv-exec-shell "/bin/zsh")
  '(realgud:pdb-command-name "python -m pdb")
  '(save-place-mode t)
  '(scroll-bar-mode nil)
- '(send-mail-function (quote mailclient-send-it))
+ '(send-mail-function 'mailclient-send-it)
  '(sentence-end-double-space nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil)
