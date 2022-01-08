b4f08baad3c512e860455f34e1c958b90595634e
Boris Shminke <boris@shminke.ml>
2022-01-08 14:30:06 +0100

    simpler basic font description

--- init.el
+++ init.el
@@ -193,5 +193,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "WhiteSmoke" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 174 :width normal :foundry "nil" :family "Menlo")))))
+ '(default ((t (:height 150)))))
 (if (window-system) (load-theme 'doom-acario-dark))
