96717f3c6a5cf93e7906b71731b61b97b199de3b
Boris Shminke <boris@shminke.ml>
2022-01-17 12:40:45 +0100

    add forge

--- init.el
+++ init.el
@@ -100,7 +100,7 @@
      ("melpa" . "https://melpa.org/packages/")
      ("org" . "https://orgmode.org/elpa/")))
  '(package-selected-packages
-   '(helpful ivy-rich doom-themes doom-modeline elpher org-trello lsp-java sbt-mode scala-mode lsp-ui lsp-ivy lsp-metals avy which-key electric-operator fireplace diff-hl hungry-delete dictionary f cdlatex move-text elpy zone-nyan nyan-mode xclip restart-emacs exec-path-from-shell disable-mouse ein ws-butler smartparens flycheck company-jedi docker py-isort counsel-projectile counsel dockerfile-mode realgud yaml-mode magit projectile markdown-mode))
+   '(forge helpful ivy-rich doom-themes doom-modeline elpher org-trello lsp-java sbt-mode scala-mode lsp-ui lsp-ivy lsp-metals avy which-key electric-operator fireplace diff-hl hungry-delete dictionary f cdlatex move-text elpy zone-nyan nyan-mode xclip restart-emacs exec-path-from-shell disable-mouse ein ws-butler smartparens flycheck company-jedi docker py-isort counsel-projectile counsel dockerfile-mode realgud yaml-mode magit projectile markdown-mode))
  '(projectile-after-switch-project-hook
    '((lambda nil
        (progn
@@ -132,8 +132,7 @@
  '(pyvenv-exec-shell
    "(if (eq system-type 'darwin) \"/bin/zsh\" \"/usr/bin/zsh\")")
  '(realgud:pdb-command-name "python -m pdb")
- '(safe-local-variable-values
-   '((projectile-project-run-cmd . "./show_report.sh")))
+ '(safe-local-variable-values '((projectile-project-run-cmd . "./show_report.sh")))
  '(save-place-mode t)
  '(scroll-bar-mode nil)
  '(send-mail-function 'mailclient-send-it)
