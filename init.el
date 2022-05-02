e73df043db0bd6361bbb42daed2c3e5fc93e7d38
Boris Shminke <boris@shminke.ml>
2022-05-02 09:58:22 +0200

    proced is not that bad

--- init.el
+++ init.el
@@ -101,6 +101,7 @@
      ("org" . "https://orgmode.org/elpa/")))
  '(package-selected-packages
    '(forge helpful ivy-rich doom-themes doom-modeline elpher org-trello lsp-java sbt-mode scala-mode lsp-ui lsp-ivy lsp-metals avy which-key electric-operator fireplace diff-hl hungry-delete dictionary f cdlatex move-text elpy zone-nyan nyan-mode xclip restart-emacs exec-path-from-shell disable-mouse ein ws-butler smartparens flycheck company-jedi docker py-isort counsel-projectile counsel dockerfile-mode realgud yaml-mode magit projectile markdown-mode))
+ '(proced-auto-update-flag t)
  '(projectile-after-switch-project-hook
    '((lambda nil
        (progn
