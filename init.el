6d70cc3601927702220b64ca0bb65ac83903a5c2
Boris Shminke <boris@shminke.com>
2023-08-27 19:41:28 +0200

    grammarly package is outdated

--- init.el
+++ init.el
@@ -112,7 +112,7 @@
      ("melpa" . "https://melpa.org/packages/")
      ("org" . "https://orgmode.org/elpa/")))
  '(package-selected-packages
-   '(lsp-java protobuf-mode elpy flymake-grammarly forge helpful ivy-rich doom-themes doom-modeline elpher org-trello sbt-mode scala-mode lsp-ui lsp-ivy lsp-metals avy which-key electric-operator fireplace diff-hl hungry-delete dictionary cdlatex move-text zone-nyan nyan-mode xclip exec-path-from-shell disable-mouse ein ws-butler smartparens flycheck company-jedi docker py-isort counsel-projectile counsel dockerfile-mode realgud yaml-mode magit projectile markdown-mode))
+   '(lsp-java protobuf-mode elpy forge helpful ivy-rich doom-themes doom-modeline elpher org-trello sbt-mode scala-mode lsp-ui lsp-ivy lsp-metals avy which-key electric-operator fireplace diff-hl hungry-delete dictionary cdlatex move-text zone-nyan nyan-mode xclip exec-path-from-shell disable-mouse ein ws-butler smartparens flycheck company-jedi docker py-isort counsel-projectile counsel dockerfile-mode realgud yaml-mode magit projectile markdown-mode))
  '(proced-auto-update-flag t)
  '(projectile-after-switch-project-hook
    '((lambda nil
