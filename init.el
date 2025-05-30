7d553d0202140b44777404bcf15be2e1a124b8bd
Boris Shminke <boris@shminke.com>
2025-05-30 19:53:21 +0200

    custom-set-faces was added by Custom.

--- init.el
+++ init.el
@@ -122,6 +122,12 @@
  '(xclip-mode t)
  '(xclip-select-enable-clipboard nil)
  '(yas-global-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 (unless package-archive-contents
   (package-refresh-contents))
 (package-install-selected-packages)
